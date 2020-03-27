3298c266ed95ba59dcd9d9e3df24ab05acd71cf3
Levi Saturnino <levisaturnino@gmail.com>
2020-03-27 00:05:16 -0300

    Autenticação & CRUD de Usuários - flash

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,9 @@
             @yield('content')
         </main>
     </div>
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
     <script>
         $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
         </script>
